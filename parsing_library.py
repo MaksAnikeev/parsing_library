5ad765de611fa8dc54e670646da15e1648bc1e14
MaksAnikeev <anikeev.maks@rambler.ru>
2022-08-23 20:49:32 +0300

    Create exception TooManyRedirects

--- parsing_library.py
+++ parsing_library.py
@@ -1,14 +1,22 @@
 import requests
 from pathlib import Path
 
+def check_for_redirect(response):
+    if response.history:
+        raise requests.TooManyRedirects
+
 Path('books').mkdir(parents=True, exist_ok = True)
 
 for i in range(10):
     url = f'https://tululu.org/txt.php?id={i+1}'
     response = requests.get(url)
-    response.raise_for_status()
-    with open(f'books/book_{i+1}.txt', 'w', encoding='UTF-8') as file:
-        file.write(response.text)
+    try:
+        check_for_redirect(response)
+        with open(f'books/book_{i+1}.txt', 'w', encoding='UTF-8') as file:
+            file.write(response.text)
+    except requests.TooManyRedirects:
+        pass
+
 
 
 
