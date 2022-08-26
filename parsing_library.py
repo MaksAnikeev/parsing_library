24cfa3a46347b037074b984b3a0ffd0c704915fd
MaksAnikeev <anikeev.maks@rambler.ru>
2022-08-26 06:31:07 +0300

    Add 'download_books' function

--- parsing_library.py
+++ parsing_library.py
@@ -1,5 +1,8 @@
+import sys
+
 import requests
 import os
+import argparse
 
 from pathlib import Path
 from bs4 import BeautifulSoup
@@ -11,11 +14,11 @@ def check_for_redirect(response):
     if response.history:
         raise requests.TooManyRedirects
 
-def download_txt(response, filename, folder='books/', number=None):
+def download_txt(response, filename, folder='books/'):
     checked_filename = sanitize_filename(filename)
     checked_folder = sanitize_filename(folder)
     Path(checked_folder).mkdir(parents=True, exist_ok=True)
-    with open(f'{checked_folder}/{number} {checked_filename}.txt', 'w', encoding='UTF-8') as file:
+    with open(f'{checked_folder}/{checked_filename}.txt', 'w', encoding='UTF-8') as file:
         file.write(response.text)
     return os.path.join(checked_folder, f'{checked_filename}.txt')
 
@@ -24,7 +27,7 @@ def download_image(url, filename, folder='image/'):
     checked_folder = sanitize_filename(folder)
     Path(checked_folder).mkdir(parents=True, exist_ok=True)
     response = requests.get(url)
-    with open(f'{checked_folder}/{checked_filename}', 'wb') as file:
+    with open(f'{checked_folder}/{checked_filename}.jpg', 'wb') as file:
         file.write(response.content)
     return os.path.join(checked_folder, f'{checked_filename}')
 
@@ -47,35 +50,41 @@ def parse_book_page(response, number):
         print(comment.text)
     print()
 
+def parse_tululu(start_id, end_id):
+    for i in range(start_id, end_id+1):
+        text_url = f'https://tululu.org/txt.php?id={i}'
+        text_response = requests.get(text_url)
+        try:
+            check_for_redirect(text_response)
 
-list=[4]
-for i in range(10):
-    text_url = f'https://tululu.org/txt.php?id={i+1}'
-    text_response = requests.get(text_url)
-    try:
-        check_for_redirect(text_response)
+            title_url = f'https://tululu.org/b{i}/'
+            title_response = requests.get(title_url)
 
-        title_url = f'https://tululu.org/b{i+1}/'
+            parse_book_page(response=title_response, number=i)
+        except requests.TooManyRedirects:
+            pass
+
+def download_books():
+    numbers = input('Введите номера через запятую: ')
+    for i in numbers.split(','):
+        title_url = f'https://tululu.org/b{i.strip()}/'
         title_response = requests.get(title_url)
+        soup = BeautifulSoup(title_response.text, 'lxml')
+        image_link = soup.find(class_='bookimage').find('img')['src']
+        full_image_link = urljoin('https://tululu.org/', image_link)
+        book_name = soup.find('table').find('h1').text
+        title_name = book_name.split('::')[0].strip()
+        download_image(url=full_image_link, filename=title_name, folder='books/')
 
-        parse_book_page(response=title_response, number=i+1)
-
-        # download_txt(response=text_response, filename=title_name, folder='books', number=i+1)
-
-        # image_link = soup.find(class_='bookimage').find('img')['src']
-        # full_image_link = urljoin('https://tululu.org/', image_link)
-        # image_name = image_link.split('/')[-1]
-        # download_image(url=full_image_link, filename=image_name, folder='image/')
-
-        # print(title_name)
-        # comments = soup.find(class_='ow_px_td').find_all(class_='black')
-        # for comment in comments:
-        #     print(comment.text)
-        # print()
-
-    except requests.TooManyRedirects:
-        pass
+        text_url = f'https://tululu.org/txt.php?id={i.strip()}'
+        text_response = requests.get(text_url)
+        download_txt(response=text_response, filename=title_name, folder='books')
 
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('start_id', type=int, help='номер с которого начать')
+    parser.add_argument('end_id', type=int, help='номер которым закончить')
+    args = parser.parse_args()
 
-
+    parse_tululu(start_id=args.start_id, end_id=args.end_id)
